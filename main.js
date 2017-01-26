54c3b3a6daa1ca4c3583a03a4244e501877ab5f2
Jessy Chung <jessyc@thentia.com>
2017-01-25 20:51:31 -0500

    init

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function getValue(value) {
+    console.log(value);
+}
+
+function addAnother() {
+    document.getElementById('wrapper').innerHTML += '<span>Label: <input type="text"><small>(ft)</small></span>\r\n';
+}
