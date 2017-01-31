2480fa5fc6b839f337f8cc9482ab32e8c73d664e
Jessy Chung <jessyc@thentia.com>
2017-01-30 21:32:44 -0500

    init

--- main.js
+++ main.js
@@ -69,17 +69,41 @@ const taxRate = [
     {
         province: 'al',
         bracket: {
-            low: 10,
-            med: 12,
-            high: 13
+            low: {
+                from: 0,
+                to: 125000,
+                rate: 10
+            },
+            med: {
+                from: 125001,
+                to: 150000,
+                rate: 12
+            },
+            high: {
+                from: 150001,
+                to: 200000,
+                rate: 13
+            }
         }
     },
     {
         province: 'on',
         bracket: {
-            low: 5.05,
-            med: 9.15,
-            high: 11.16
+            low: {
+                from: 0,
+                to: 41536,
+                rate: 5.05
+            },
+            med: {
+                from: 41537,
+                to: 83075,
+                rate: 9.15
+            },
+            high: {
+                from: 83076,
+                to: 150000,
+                rate: 11.16
+            }
         }
     }
 ];
@@ -108,31 +132,46 @@ function showResult() {
 
     income = parseFloat(document.getElementById('inputIncome').value);
 
-    //get low med or high
-
-    if(income < 45282){
-
-    }
-
     for(item in taxRate) {
-        var getProvince = taxRate[item].province;
-        if(province === getProvince) {
 
+        var getProvince = taxRate[item].province;
+
+        if(province === getProvince) {
 
             //calc fed tax
             var fedTax;
             if(income < 45282){
                 fedTax = income * (15/100);
-                console.log(fedTax)
+                console.log('low: '+ fedTax);
             } else if(income > 45282 && income < 90563) {
-                fedTax = income * (20.5/100);
-                console.log(fedTax)
-            } else {
-
+                var lowLeftover = income - 45282;
+                var medLeftover = lowLeftover * (20.5/100);
+                fedTax = lowLeftover + medLeftover;
+                console.log('med:' + fedTax);
+            } else if(income > 90536 && income < 140388) {
+                var medLeftover = income - 90536;
+                var highLeftover = medLeftover * (26/100);
+                fedTax = medLeftover + highLeftover;
+                console.log('high: ' + fedTax);
             }
 
+            var lowTo = taxRate[item].bracket.low.to;
+            var medFrom = taxRate[item].bracket.med.from;
+            var medTo = taxRate[item].bracket.med.to;
+            var highFrom = taxRate[item].bracket.high.from;
+            var highTo = taxRate[item].bracket.high.to;
+
+
             //calc provincial tax
-            tax = taxRate[item].bracket.low;
+            var provincialTax;
+            if(income < lowTo ){
+                provincialTax = income * (taxRate[item].bracket.low.rate / 100)
+            } else if (income > lowTo && income < medFrom) {
+                var lowLeftover = income - lowTo;
+                var medLeftover = lowLeftover * (taxRate[item].bracket.low.rate/100);
+            }
+
+
         }
     }
 
